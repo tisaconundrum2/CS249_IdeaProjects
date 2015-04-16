95abd5d7010ce9b4ba1fd8332c74de6d10a67f14
Nicholas Finch <ngf4@nau.edu>
2015-04-16 11:56:59 -0700

    what eveN!

--- CS 249 - hashTree/src/com/company/hashAppTree.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-/**
- * Created by User on 11/24/2014.
- */
-class hashAppTree {
-    
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# CS249_IdeaProjects
-All the projects ever for CS 249, be responsible with them ;)
